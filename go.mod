ad30b52933e757b78e9eda72c0f087e4c2e76d7d
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-04-07 12:27:38 +0700

    GO-MODULE-CONSUME: Added New Version for Go Module Sample Consumption

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/TantowiPutra/Go-App-Say-Hello
 
 go 1.19
 
-require github.com/TantowiPutra/Go-Module v1.0.1
+require github.com/TantowiPutra/Go-Module v1.0.2
+
+require github.com/TantowiPutra/Go-Module/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	sample "github.com/TantowiPutra/Go-Module"
+	sample2 "github.com/TantowiPutra/Go-Module/v2"
 )
 
 func main() {
-	fmt.Println(sample.SayHello("Tantowi"))
+	number, text := sample.SayHello("Tantowi")
+	fmt.Println("Number:", number)
+	fmt.Println("Text:", text)
+
+	text2 := sample2.SayHello("Tantowi", "Putra")
+	fmt.Println(text2)
 }
